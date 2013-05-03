351595f8127fb1c6e93af232305511ff0f3c3d46
Andrew Sardone <andrew@andrewsardone.com>
2013-05-03 13:38:27 -0400

    Add empty application Xcode template project

---
+++ XctoolCocoaPodsTests/XctoolCocoaPodsTests.m
@@ -0,0 +1,29 @@
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface XctoolCocoaPodsTests : SenTestCase
+
+@end
+
+
+@implementation XctoolCocoaPodsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in XctoolCocoaPodsTests");
+}
+
+@end
